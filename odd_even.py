403f2cd4b3a84895dea302c855c163b8638fb23b
abhinav <abhinavlohani@gmail.com>
2023-03-21 06:52:13 +0530

    updating script

--- odd_even.py
+++ odd_even.py
@@ -1,10 +1,6 @@
-def odd_even(a):
-    if a % 2 == 0:
-        print(str(a) + " is odd")
-    else:
-        print(str(a) + " is even")
-
+def printnumbers(b, a):
+    for x in range(a):
+        print(f'{b} : {x}')
 
 if __name__ == '__main__':
-    for x in range(10):
-        odd_even(x)
+    printnumbers("The number is " ,10)
